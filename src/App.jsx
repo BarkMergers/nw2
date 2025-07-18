b820f0d309a960bf22d5c30eddfdcae3aa481324
Mark Burgess <mburgess1983@gmail.com>
2025-07-18 12:58:39 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">Tailwind is working!</h1>
+    </div>
+  );
+}
